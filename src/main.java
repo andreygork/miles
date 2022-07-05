579402443d7bf4c08155dd388c311189db4405ab
andreygork <agorkaev99@gmail.com>
2022-07-05 20:37:20 +0300

    bonus

---
+++ src/main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args) {
+
+        int amount = 4550;
+        int miles = 20;
+        int bonus = amount / miles;
+        System.out.println("Начислено " + bonus +   " миль");
+    }
+}
